2626e3fdf71786f4f648a01636475a5e56724fb4
tkjbi <6429078@gmail.com>
2019-01-14 15:32:39 +0200

    עע

---
+++ BE/XmlSample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BE
+{
+    class XmlSample
+    {
+        XElement studentRoot;
+
+        string studentPath = @"StudentXml.xml";
+    }
+}
